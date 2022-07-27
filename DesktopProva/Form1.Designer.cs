cfbe9deec1c28f280237c7cd1f8c7ab44689fd01
Ronaldo Sobrinho <rpvsobrinho@gmail.com>
2022-07-27 16:14:08 -0300

    Ebook

---
+++ DesktopProva/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace DesktopProva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txttitulo = new System.Windows.Forms.TextBox();
+            this.txtautor = new System.Windows.Forms.TextBox();
+            this.txtcategoria = new System.Windows.Forms.TextBox();
+            this.listlivro = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Editar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Título:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Autor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Categoria";
+            // 
+            // txttitulo
+            // 
+            this.txttitulo.Location = new System.Drawing.Point(205, 54);
+            this.txttitulo.Name = "txttitulo";
+            this.txttitulo.Size = new System.Drawing.Size(257, 26);
+            this.txttitulo.TabIndex = 6;
+            this.txttitulo.TextChanged += new System.EventHandler(this.txttitulo_TextChanged);
+            // 
+            // txtautor
+            // 
+            this.txtautor.Location = new System.Drawing.Point(205, 86);
+            this.txtautor.Name = "txtautor";
+            this.txtautor.Size = new System.Drawing.Size(257, 26);
+            this.txtautor.TabIndex = 7;
+            // 
+            // txtcategoria
+            // 
+            this.txtcategoria.Location = new System.Drawing.Point(205, 118);
+            this.txtcategoria.Name = "txtcategoria";
+            this.txtcategoria.Size = new System.Drawing.Size(257, 26);
+            this.txtcategoria.TabIndex = 8;
+            // 
+            // listlivro
+            // 
+            this.listlivro.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listlivro.HideSelection = false;
+            this.listlivro.Location = new System.Drawing.Point(10, 233);
+            this.listlivro.MultiSelect = false;
+            this.listlivro.Name = "listlivro";
+            this.listlivro.Size = new System.Drawing.Size(620, 480);
+            this.listlivro.TabIndex = 9;
+            this.listlivro.UseCompatibleStateImageBehavior = false;
+            this.listlivro.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listlivro_ItemSelectionChanged);
+            this.listlivro.SelectedIndexChanged += new System.EventHandler(this.listlivro_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.Location = new System.Drawing.Point(222, 192);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(240, 26);
+            this.txtbuscar.TabIndex = 12;
+            this.txtbuscar.TextChanged += new System.EventHandler(this.txtbuscar_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(176, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(257, 32);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "EBOOK DESKTOP";
+            // 
+            // Editar
+            // 
+            this.Editar.Location = new System.Drawing.Point(247, 150);
+            this.Editar.Name = "Editar";
+            this.Editar.Size = new System.Drawing.Size(92, 33);
+            this.Editar.TabIndex = 14;
+            this.Editar.Text = "Limpar";
+            this.Editar.UseVisualStyleBackColor = true;
+            this.Editar.Click += new System.EventHandler(this.Editar_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 33);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Deletar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(642, 725);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Editar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtbuscar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listlivro);
+            this.Controls.Add(this.txtcategoria);
+            this.Controls.Add(this.txtautor);
+            this.Controls.Add(this.txttitulo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ebook Desktop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txttitulo;
+        private System.Windows.Forms.TextBox txtautor;
+        private System.Windows.Forms.TextBox txtcategoria;
+        private System.Windows.Forms.ListView listlivro;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Editar;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DesktopProva/Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DesktopProva
+{
+
+    public partial class Form1 : Form
+    {
+        private MySqlConnection Conexao;
+        private string data_source = "datasource=localhost;username=root;password=;database=DesktopProva";
+        private int? idSelecionado = null;
+        public Form1()
+        {
+            InitializeComponent();
+            carregar();
+            listlivro.View = View.Details;
+            listlivro.LabelEdit = true;
+            listlivro.AllowColumnReorder = true;
+            listlivro.FullRowSelect = true;
+            listlivro.GridLines = true;
+
+
+            listlivro.Columns.Add("Id", 60, HorizontalAlignment.Center);
+            listlivro.Columns.Add("Titulo", 100, HorizontalAlignment.Center);
+            listlivro.Columns.Add("Autor", 100, HorizontalAlignment.Center);
+            listlivro.Columns.Add("Categoria", 150, HorizontalAlignment.Center);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+
+                string autor = txtautor.Text;
+                string titulo = txttitulo.Text;
+                string categoria = txtcategoria.Text;
+
+                if (titulo == "")
+                {
+                    MessageBox.Show("Favor informar o título", "Título", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (autor == "")
+                {
+                    MessageBox.Show("Favor informar o autor", "Autor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (categoria == "")
+                {
+                    MessageBox.Show("Favor informar a categoria", "Categoria", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+
+
+
+
+
+                    Conexao = new MySqlConnection(data_source);
+                    Conexao.Open();
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = Conexao;
+
+
+
+
+                    if (idSelecionado == null)
+                    {
+
+                        //salvar
+                        cmd.Prepare();
+                        cmd.Parameters.AddWithValue("titulo", titulo);
+                        cmd.Parameters.AddWithValue("autor", autor);
+                        cmd.Parameters.AddWithValue("categoria", categoria);
+
+
+
+                        cmd.CommandText = "INSERT INTO base (titulo, autor, categoria) " +
+                                     " VALUES (@titulo, @autor, @categoria)";
+
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Livro inserido com sucesso!", "Inserido", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                        txtautor.Clear();
+                        txttitulo.Clear();
+                        txtcategoria.Clear();
+                        carregar();
+                    }
+                    else
+                    {
+                        //atualizar
+                        cmd.Prepare();
+                        cmd.Parameters.AddWithValue("titulo", titulo);
+                        cmd.Parameters.AddWithValue("autor", autor);
+                        cmd.Parameters.AddWithValue("categoria", categoria);
+                        cmd.Parameters.AddWithValue("id", idSelecionado);
+
+                        cmd.CommandText = "UPDATE base SET titulo = @titulo, autor = @autor, categoria = @categoria WHERE id = @id ";
+
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Livro editado com sucesso!");
+                        idSelecionado = null;
+                        txtautor.Clear();
+                        txttitulo.Clear();
+                        txtcategoria.Clear();
+                        carregar();
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtbuscar.Text == "")
+                {
+                    MessageBox.Show("Favor informar o que deseja pesquisar", "Buscar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                string buscar = txtbuscar.Text;
+
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao;
+                cmd.Prepare();
+
+                cmd.CommandText = "SELECT * FROM base WHERE titulo LIKE @q OR autor LIKE @q OR categoria LIKE @q";
+                cmd.Parameters.AddWithValue("@q", "%" + buscar + "%");
+
+
+
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+
+                listlivro.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+                    listlivro.Items.Add(new ListViewItem(row));
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void carregar()
+        {
+            try
+            {
+                string buscar = txtbuscar.Text;
+
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao;
+                cmd.CommandText = "SELECT * FROM base ORDER BY id DESC";
+                cmd.Prepare();
+
+
+
+
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+
+                listlivro.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+                    listlivro.Items.Add(new ListViewItem(row));
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void listlivro_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            ListView.SelectedListViewItemCollection selecionar = listlivro.SelectedItems;
+
+            foreach (ListViewItem seleciona in selecionar)
+            {
+                idSelecionado = Convert.ToInt32(seleciona.SubItems[0].Text);
+                txttitulo.Text = seleciona.SubItems[1].Text;
+                txtautor.Text = seleciona.SubItems[2].Text;
+                txtcategoria.Text = seleciona.SubItems[3].Text;
+            }
+
+        }
+
+        private void Editar_Click(object sender, EventArgs e)
+        {
+            idSelecionado = null;
+
+            txtautor.Clear();
+            txttitulo.Clear();
+            txtcategoria.Clear();
+            txtbuscar.Clear();
+            button3.Visible = false;
+            carregar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                DialogResult conf = MessageBox.Show("Deseja Excluir?", "Excluir", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (conf == DialogResult.Yes)
+                {
+
+                    Conexao = new MySqlConnection(data_source);
+                    Conexao.Open();
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = Conexao;
+
+                    cmd.Parameters.AddWithValue("id", idSelecionado);
+
+                    cmd.CommandText = "DELETE FROM base WHERE id = @id ";
+
+                    cmd.ExecuteNonQuery();
+
+
+                    MessageBox.Show("Livro excluído", "Excluído", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    carregar();
+                    button3.Visible = false;
+                    txtautor.Clear();
+                    txttitulo.Clear();
+                    txtcategoria.Clear();
+                    idSelecionado = null;
+                }
+                else
+                {
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+
+
+        }
+
+        private void listlivro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button3.Visible = true;
+            if (listlivro.SelectedItems.Count < 0)
+            {
+                string autor = txtautor.Text;
+                string titulo = txttitulo.Text;
+                string categoria = txtcategoria.Text;
+
+                if (titulo == "")
+                {
+                    MessageBox.Show("Favor informar o título", "Título", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (autor == "")
+                {
+                    MessageBox.Show("Favor informar o autor", "Autor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (categoria == "")
+                {
+                    MessageBox.Show("Favor informar a categoria", "Categoria", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+
+
+
+
+
+                    Conexao = new MySqlConnection(data_source);
+                    Conexao.Open();
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = Conexao;
+
+                    //salvar
+                    cmd.Prepare();
+                    cmd.Parameters.AddWithValue("titulo", titulo);
+                    cmd.Parameters.AddWithValue("autor", autor);
+                    cmd.Parameters.AddWithValue("categoria", categoria);
+
+
+
+                    cmd.CommandText = "INSERT INTO base (titulo, autor, categoria) " +
+                                 " VALUES (@titulo, @autor, @categoria)";
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Livro inserido com sucesso!", "Inserido", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                    txtautor.Clear();
+                    txttitulo.Clear();
+                    txtcategoria.Clear();
+                    carregar();
+                }
+            }
+        }
+
+        private void txtbuscar_TextChanged(object sender, EventArgs e)
+        {
+            if (txtbuscar.Text == "")
+            {
+                carregar();
+            }
+        }
+
+        private void txttitulo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
